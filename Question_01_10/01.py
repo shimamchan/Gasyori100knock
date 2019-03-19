088702d9e17435f135b40dc8668d509cb0b8315b
shimamchan <shimamuko.akane@tesyio.com>
2019-03-19 23:50:05 +0900

    Add 01.py out_01.py

---
+++ Question_01_10/01.py
@@ -0,0 +1,15 @@
+import cv2
+
+img = cv2.imread("imori.jpg")
+b = img[:, :, 0].copy()
+g = img[:, :, 1].copy()
+r = img[:, :, 2].copy()
+
+img[:, :, 0] = r
+img[:, :, 1] = g
+img[:, :, 2] = b
+
+cv2.imwrite("out_01.jpg", img)
+cv2.imshow("result", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
